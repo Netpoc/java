22bc893269add342460952f8b5d4f3df87f0588d
Ayenoto Peter <ayenotopeter@hotmail.com>
2022-10-16 01:07:37 +0000

    Setup Complete

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,18 +1,13 @@
+
 /*
- 
 This class returns :
 Day of the week,
 Print message to console,
 Sum of all even numbers less than 1000,
 Sum of even numbers between 12 and 103
-
 */
 public class TryingMethods {
     public static void main(String[] args) {
-        int x = 20, y = 18;
-        if (x > y) {
-            System.out.println("x is greater tha y");
-        }
-    }
-        
+        System.out.println("Assignment 1");
+    }        
 }
