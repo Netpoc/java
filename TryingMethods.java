551d3633cb5641fc305d84732773e82afb22b851
Ayenoto Peter <ayenotopeter@hotmail.com>
2022-10-17 19:50:05 +0000

    Sum of Even Numbers

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 /*
 This class returns :
@@ -7,7 +8,19 @@ Sum of all even numbers less than 1000,
 Sum of even numbers between 12 and 103
 */
 public class TryingMethods {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
         System.out.println("Assignment 1");
-    }        
+        sumOfEvenNumber();
+    }
+    
+    public static void sumOfEvenNumber() {
+        int sum = 0;
+        for(int i = 12; i <= 103; i++) {
+            if(i % 2 == 0) {
+                sum += i;          
+            }
+            
+        }
+        System.out.println(sum);
+    }
 }
