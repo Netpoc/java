affb111330f1a586bab55692afef0d007025ccad
Ayenoto Peter <ayenotopeter@hotmail.com>
2022-10-18 08:51:45 +0000

    Progress with sumOfEvenNumbers

--- TryingMethods.java
+++ TryingMethods.java
@@ -10,10 +10,10 @@ Sum of even numbers between 12 and 103
 public class TryingMethods {
     public static void main(String[] args) {    
         System.out.println("Assignment 1");
-        sumOfEvenNumber();
+        
     }
     
-    public static void sumOfEvenNumber() {
+    public int sumOfEvenNumber(int a) {
         int sum = 0;
         for(int i = 12; i <= 103; i++) {
             if(i % 2 == 0) {
@@ -21,6 +21,6 @@ public class TryingMethods {
             }
             
         }
-        System.out.println(sum);
+        return sum;
     }
 }
