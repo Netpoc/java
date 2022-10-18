d13623f878149f59431d0aad624a8e21813cfc5e
Ayenoto Peter <ayenotopeter@hotmail.com>
2022-10-18 06:49:50 +0000

    Days of the Week

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,5 +1,3 @@
-import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
-
 /*
 This class returns :
 Day of the week,
@@ -11,6 +9,8 @@ public class TryingMethods {
     public static void main(String[] args) {    
         System.out.println("Assignment 1");
         sumOfEvenNumber();
+        TryingMethods weekly = new TryingMethods();
+        weekly.daysOfTheWeek(4);
     }
     
     public static void sumOfEvenNumber() {
@@ -18,9 +18,40 @@ public class TryingMethods {
         for(int i = 12; i <= 103; i++) {
             if(i % 2 == 0) {
                 sum += i;          
-            }
-            
+            }   
         }
-        System.out.println(sum);
+        System.out.println("Sum of even numbers between 12 and 103: " + sum);
     }
+
+    public String daysOfTheWeek(int a) {
+        String day = "";
+        switch (a) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+
+            default:
+                break;
+        }
+        return day;
+    }
+
 }
