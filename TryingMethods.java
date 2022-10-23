bb23cb5d3c0ff17afbf6502f5e7d03dcc8ddc702
Ayenoto Peter <ayenotopeter@hotmail.com>
2022-10-23 06:04:38 +0000

    Fresh starts

--- TryingMethods.java
+++ TryingMethods.java
@@ -9,37 +9,7 @@ Sum of even numbers between 12 and 103
 public class TryingMethods {
     public static void main(String[] args) {
         System.out.println("Assignment 1");
-        daysOfTheWeek(3);
-    }
-
-    static void daysOfTheWeek(int day) {
         
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-                
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                break;
-        }
     }
     
 }
